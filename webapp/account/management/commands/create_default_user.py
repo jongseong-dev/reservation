e9dc40851bf64bcc833f4f08e889b5717dcb114c
jslee <dlwhdtjd098@gmail.com>
2024-07-22 10:49:56 +0900

    feat(account.commands): local환경에서 관리자와 고객을 기본적으로 넣는 command 추가

---
+++ webapp/account/management/commands/create_default_user.py
@@ -0,0 +1,60 @@
+# yourapp/management/commands/create_default_users.py
+import os
+
+from django.contrib.auth.hashers import make_password
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+from account.models import Company
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = """
+    Creates default superuser and regular user.
+    Run this command in local env
+    """
+
+    def handle(self, *args, **kwargs):
+        env = os.environ.get("DJANGO_SETTINGS_MODULE")
+        if "local" not in env:
+            self.stdout.write(
+                self.style.ERROR(
+                    "This command can only be run in local environment"
+                )
+            )
+            return
+
+        admin_password = make_password("adminpassword")
+        user_password = make_password("userpassword")
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                "admin", "admin@example.com", admin_password
+            )
+            self.stdout.write(
+                self.style.SUCCESS("Superuser created successfully")
+            )
+        else:
+            self.stdout.write(self.style.WARNING("Superuser already exists"))
+
+        if not User.objects.filter(username="user").exists():
+            company, _ = Company.objects.get_or_create(
+                name="example company",
+                email="company@example.com",
+                established_date="2022-01-01",
+            )
+            User.objects.create_user(
+                "user",
+                "user@example.com",
+                user_password,
+                company=company,
+            )
+            self.stdout.write(
+                self.style.SUCCESS("Regular user created successfully")
+            )
+        else:
+            self.stdout.write(
+                self.style.WARNING("Regular user already exists")
+            )
