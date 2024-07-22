fc54f2b9f6dfa068fe29b903b5a0a61b0c6e8217
jslee <dlwhdtjd098@gmail.com>
2024-07-22 20:43:19 +0900

    feat(urls): jwt token api url 추가

--- webapp/config/urls.py
+++ webapp/config/urls.py
@@ -13,23 +13,39 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
 from django.http import JsonResponse
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 
 def health_check(request):
-    return JsonResponse({'status': 'ok'})
+    return JsonResponse({"status": "ok"})
 
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
-    path('admin/', admin.site.urls),
-    path('health/', health_check, name='health_check'),
+    path("api-auth/", include("rest_framework.urls")),
+    path("admin/", admin.site.urls),
+    path("health/", health_check, name="health_check"),
+    path(
+        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
 
 if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
-        urlpatterns += [path("__debug__/", include("debug_toolbar.urls")), ]
+        urlpatterns += [
+            path("__debug__/", include("debug_toolbar.urls")),
+        ]
