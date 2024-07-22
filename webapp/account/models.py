06157c00c7bcfbce0c931a91aeebed0e3653e24e
jslee <dlwhdtjd098@gmail.com>
2024-07-22 10:48:22 +0900

    feat(account.models): 사용자와 회사 추가
    1. 사용자 모델 추가
    2. 회사 모델 추가

--- webapp/account/models.py
+++ webapp/account/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100, db_comment="회사 이름")
+    email = models.EmailField(max_length=200, unique=True, db_comment="회사 이메일")
+    established_date = models.DateField(db_comment="설립일")
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        max_length=200, unique=True, db_comment="사용자 이메일"
+    )
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="users",
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
