4bfbb5289a7e317e628479112ca696a46b3ea219
jslee <dlwhdtjd098@gmail.com>
2024-07-23 23:55:16 +0900

    feat(reservations.api): 예약 신청을 위한 api 추가 및 api docs 추가

--- webapp/reservation/api_schemas.py
+++ webapp/reservation/api_schemas.py
@@ -1,4 +1,7 @@
-from drf_spectacular.utils import OpenApiParameter
+import datetime
+from django.utils import timezone
+
+from drf_spectacular.utils import OpenApiParameter, OpenApiExample
 from drf_spectacular.types import OpenApiTypes
 
 reservation_list_parameters = [
@@ -9,5 +12,52 @@ reservation_list_parameters = [
         description="API version",
         enum=["v1", "v2"],
         default="v1",
+    ),
+]
+reservation_apply_example = [
+    OpenApiExample(
+        "유효한 예약 요청",
+        summary="유효한 예약 요청의 예시",
+        description="이 예시는 시작 및 종료 일시와 예약 인원 수를 포함한 올바른 예약 요청 형식을 보여줍니다.",
+        value={
+            "reserved_start_datetime": (
+                timezone.now() + datetime.timedelta(days=3, hours=10)
+            ).strftime("%Y-%m-%dT%H:00:00+0900"),
+            "reserved_end_datetime": (
+                timezone.now() + datetime.timedelta(days=3, hours=14)
+            ).strftime("%Y-%m-%dT%H:00:00+0900"),
+            "reserved_count": 2,
+        },
+        request_only=True,
+    ),
+    OpenApiExample(
+        "유효하지 않은 예약 요청 1",
+        summary="끝일이 시작일보다 이른 유효하지 않은 예약 요청",
+        description="이 예시는 종료 시간이 시작 시간보다 이른 잘못된 요청을 보여줍니다.",
+        value={
+            "reserved_start_datetime": (
+                timezone.now() + datetime.timedelta(days=3, hours=5)
+            ).strftime("%Y-%m-%dT%H:00:00+0900"),
+            "reserved_end_datetime": (
+                timezone.now() + datetime.timedelta(days=3)
+            ).strftime("%Y-%m-%dT%H:00:00+0900"),
+            "reserved_count": 6,
+        },
+        request_only=True,
+    ),
+    OpenApiExample(
+        "유효하지 않은 예약 요청 2",
+        summary="2일전에 예약하려고 하는 유효하지 않은 예약",
+        description="3일 전까지만 예약이 가능합니다.",
+        value={
+            "reserved_start_datetime": (
+                timezone.now() + datetime.timedelta(days=2, hours=5)
+            ).strftime("%Y-%m-%dT%H:00:00+0900"),
+            "reserved_end_datetime": (
+                timezone.now() + datetime.timedelta(days=3)
+            ).strftime("%Y-%m-%dT%H:00:00+0900"),
+            "reserved_count": 6,
+        },
+        request_only=True,
     ),
 ]
--- webapp/reservation/views.py
+++ webapp/reservation/views.py
@@ -1,19 +1,32 @@
 from drf_spectacular.utils import extend_schema, OpenApiResponse
-from rest_framework import mixins
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import GenericViewSet
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 from reservation.api_schemas import (
     reservation_list_parameters,
+    reservation_apply_example,
 )
 from reservation.models import Reservation
-from reservation.serializers import ReservationListSerializer
+from reservation.serializers import (
+    ReservationListSerializer,
+    ReservationCreateSerializer,
+    ReservationSerializer,
+)
 
 
-class ReservationListViewSet(mixins.ListModelMixin, GenericViewSet):
+class ReservationViewSet(viewsets.ModelViewSet):
     queryset = Reservation.reserved.all()
-    serializer_class = ReservationListSerializer
+    # serializer_class = ReservationListSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return ReservationCreateSerializer
+        elif self.action in ["update", "partial_update"]:
+            pass
+        return ReservationListSerializer
 
     @extend_schema(
         summary="예약 가능한 일자를 조회하기 위한 API",
@@ -29,3 +42,23 @@ class ReservationListViewSet(mixins.ListModelMixin, GenericViewSet):
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="예약 신청할 수 있는 API",
+        description="""
+                고객이 해당 일자에 예약 신청하는 API
+                """,
+        tags=["reservation"],
+        request=ReservationCreateSerializer,
+        examples=reservation_apply_example,
+        responses={
+            200: OpenApiResponse(response=ReservationListSerializer),
+        },
+    )
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        results = serializer.save(user=request.user)
+        reservation_data = ReservationSerializer(results, many=True).data
+
+        return Response(reservation_data, status=status.HTTP_201_CREATED)
