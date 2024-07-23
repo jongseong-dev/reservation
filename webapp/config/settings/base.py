917c8547a65645db2bd4de77343f0db6520b0e26
jslee <dlwhdtjd098@gmail.com>
2024-07-23 23:46:05 +0900

    feat(config): redis 설정 추가

--- webapp/config/settings/base.py
+++ webapp/config/settings/base.py
@@ -181,3 +181,21 @@ SPECTACULAR_SETTINGS = {
     "SERVE_INCLUDE_SCHEMA": False,
     # OTHER SETTINGS
 }
+
+
+# Redis
+
+# Redis 설정
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
+REDIS_DB = os.environ.get("REDIS_DB", 1)
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}",  # Redis 서버 주소와 DB 번호
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
