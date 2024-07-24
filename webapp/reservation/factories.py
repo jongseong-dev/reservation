67b4a827c302b58ce1b9a5e9b42c854ab990f935
jslee <dlwhdtjd098@gmail.com>
2024-07-24 11:24:18 +0900

    modify(reservation.factories): 모델 변경에 따른 factory class 추가 및 수정

--- webapp/reservation/factories.py
+++ webapp/reservation/factories.py
@@ -2,17 +2,30 @@ import factory
 from factory.django import DjangoModelFactory
 
 from .const import MAXIMUM_RESERVED_COUNT
-from .models import Reservation
+from .models import Reservation, ExamSchedule
 from account.factories import UserFactory
+
+
+class ExamScheduleFactory(DjangoModelFactory):
+    class Meta:
+        model = ExamSchedule
+
+    start_datetime = factory.Faker("date_time_this_month")
+    end_datetime = factory.Faker("date_time_this_month", before_now=False)
+    max_capacity = factory.Faker(
+        "random_int", min=1, max=MAXIMUM_RESERVED_COUNT
+    )
+    confirmed_reserved_count = factory.Faker(
+        "random_int", min=0, max=MAXIMUM_RESERVED_COUNT
+    )
 
 
 class ReservationFactory(DjangoModelFactory):
     class Meta:
         model = Reservation
 
-    reserved_datetime = factory.Faker("future_datetime")
     user = factory.SubFactory(UserFactory)
+    exam_schedule = factory.SubFactory(ExamSchedule)
     reserved_count = factory.Faker(
         "random_int", min=1, max=MAXIMUM_RESERVED_COUNT
     )
-    status = factory.Iterator(Reservation.Status, getter=lambda c: c[0])
