32f703ff82a281d5593fb0f4525b5ef88595a669
jongseong-keep-going-dev <60998945+jongseong-dev@users.noreply.github.com>
2024-07-24 20:30:33 +0900

    시험 예약 신청 API 추가 (#17)
    * update(pre-commit): 버전 업데이트 및 pyright 수정
    
    * feat(remainder): 캐시를 통한 남은 예약인원을 확인하기 위한 클래스 추가
    
    * modify(reservations.api): 예약 신청 API 수정
    
    * test: test 코드 추가
    
    * style: 필요없는 import 삭제
    
    * test: 테스트 코드 수정
    
    * modify(test): ExamScheduleFactory의 max_capacity 할당 숫자 수정

--- webapp/reservation/api_schemas.py
+++ webapp/reservation/api_schemas.py
@@ -1,6 +1,3 @@
-import datetime
-from django.utils import timezone
-
 from drf_spectacular.utils import OpenApiExample
 
 reservation_apply_example = [
@@ -9,43 +6,19 @@ reservation_apply_example = [
         summary="유효한 예약 요청의 예시",
         description="이 예시는 시작 및 종료 일시와 예약 인원 수를 포함한 올바른 예약 요청 형식을 보여줍니다.",
         value={
-            "reserved_start_datetime": (
-                timezone.now() + datetime.timedelta(days=3, hours=10)
-            ).strftime("%Y-%m-%dT%H:00:00+0900"),
-            "reserved_end_datetime": (
-                timezone.now() + datetime.timedelta(days=3, hours=14)
-            ).strftime("%Y-%m-%dT%H:00:00+0900"),
+            "exam_schedule_id": 1,
             "reserved_count": 2,
         },
         request_only=True,
     ),
     OpenApiExample(
         "유효하지 않은 예약 요청 1",
-        summary="끝일이 시작일보다 이른 유효하지 않은 예약 요청",
-        description="이 예시는 종료 시간이 시작 시간보다 이른 잘못된 요청을 보여줍니다.",
+        summary="예약 인원 수가 정해진 범위를 벗어난 경우",
+        description="현재 50,000명이 최대 예약 가능 인원으로 설정되어 있습니다."
+        "예약 가능 인원은 최대 예약 가능인원 - 확정된 예약 인원 입니다.",
         value={
-            "reserved_start_datetime": (
-                timezone.now() + datetime.timedelta(days=3, hours=5)
-            ).strftime("%Y-%m-%dT%H:00:00+0900"),
-            "reserved_end_datetime": (
-                timezone.now() + datetime.timedelta(days=3)
-            ).strftime("%Y-%m-%dT%H:00:00+0900"),
-            "reserved_count": 6,
-        },
-        request_only=True,
-    ),
-    OpenApiExample(
-        "유효하지 않은 예약 요청 2",
-        summary="2일전에 예약하려고 하는 유효하지 않은 예약",
-        description="3일 전까지만 예약이 가능합니다.",
-        value={
-            "reserved_start_datetime": (
-                timezone.now() + datetime.timedelta(days=2, hours=5)
-            ).strftime("%Y-%m-%dT%H:00:00+0900"),
-            "reserved_end_datetime": (
-                timezone.now() + datetime.timedelta(days=3)
-            ).strftime("%Y-%m-%dT%H:00:00+0900"),
-            "reserved_count": 6,
+            "exam_schedule_id": 1,
+            "reserved_count": 51000,
         },
         request_only=True,
     ),
--- webapp/tests/reservation/test_views.py
+++ webapp/tests/reservation/test_views.py
@@ -2,9 +2,18 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 
+from reservation.const import (
+    MAXIMUM_RESERVED_COUNT,
+)
+
+
+@pytest.fixture
+def apply_reservation_url():
+    return reverse("reservation:reservation-list", kwargs={"version": "v1"})
+
 
 @pytest.mark.django_db
-def test_access_reservation_list_auth_user(auth_user_client, exam_schedule):
+def test_exam_schedule_list_api_auth_user(auth_user_client, exam_schedule):
     response = auth_user_client.get(
         reverse("reservation:exam-schedule-list", kwargs={"version": "v1"})
     )
@@ -20,5 +29,56 @@ def test_access_reservation_list_auth_user(auth_user_client, exam_schedule):
 def test_access_exam_schedule_anonymous(client):
     response = client.get(
         reverse("reservation:exam-schedule-list", kwargs={"version": "v1"})
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.django_db
+def test_apply_reservation_api_reserves_correctly(
+    auth_user_client, apply_reservation_url, exam_schedule, reservation
+):
+    response = auth_user_client.post(
+        apply_reservation_url,
+        {"exam_schedule_id": exam_schedule.id, "reserved_count": 30},
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_apply_reservation_api_raises_error_for_exceeding_capacity(
+    auth_user_client, apply_reservation_url, exam_schedule, reservation
+):
+    response = auth_user_client.post(
+        apply_reservation_url,
+        {
+            "exam_schedule_id": exam_schedule.id,
+            "reserved_count": MAXIMUM_RESERVED_COUNT + 300,
+        },
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_apply_reservation_api_raises_error_for_nonexistent_schedule(
+    auth_user_client, apply_reservation_url, exam_schedule, reservation
+):
+
+    response = auth_user_client.post(
+        apply_reservation_url,
+        {
+            "exam_schedule_id": exam_schedule.id + 100,
+            "reserved_count": MAXIMUM_RESERVED_COUNT + 300,
+        },
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_apply_reservation_anonymous(
+    client, apply_reservation_url, exam_schedule, reservation
+):
+    response = client.post(
+        apply_reservation_url,
+        {"exam_schedule_id": exam_schedule.id, "reserved_count": 30},
     )
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
