a72935fb9da4c415ce6cfce83f2ca9175f1ef75e
jslee <dlwhdtjd098@gmail.com>
2024-07-24 15:31:43 +0900

    feat(remainder): 캐시를 통한 남은 예약인원을 확인하기 위한 클래스 추가

---
+++ webapp/reservation/remainder.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from django.core import cache
+
+
+class Remainder:
+    """
+    캐시를 통한 예약 가능 인원수를 계산하는 클래스
+    """
+
+    def __init__(self):
+        self.storage = cache
+
+    def get_remain_count(self, key: str) -> int | None:
+        """
+        남은 예약 가능 인원 수를 조회
+        """
+        return self.storage.get(key, None)
+
+    def update_remain_count(self, key: str, total: int, reserved: int) -> int:
+        """
+        예약 가능 인원 수를 업데이트
+        """
+        self.storage.set(key, total - reserved)
+        return total - reserved
+
+
+remainder_count = Remainder()
